4acfb447c78755293c9c6827d6fcdc2f943f62bd
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2022-10-10 09:01:23 +0530

    approach to find middle element made better.

--- binary2.cpp
+++ binary2.cpp
@@ -1,32 +1,42 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int search(int arr[],int n,int key)
+int search(int arr[], int n, int key)
 {
-    int s=0;
-    int e=n-1;
-    while(s<=e)
+    int s = 0;
+    int e = n - 1;
+    while (s <= e)
     {
-        int mid=(s+e)/2;
-        if(arr[mid]==key)
-        return mid;
-        else if(arr[mid]>key)
+        // This is not a better approach as is start, i.e, 's' and end, i.e., 'e' will be very large integers than
+        // our start+end, i.e., (s+e) assigned to out variable 'mid' will go beyond the range of integers.
+        // int mid=(s+e)/2;
+        // Hence a better approach to create 'mid' variable is as follows
+        int mid = s + (e - s) / 2;
+
+        if (arr[mid] == key)
+            return mid;
+        else if (arr[mid] > key)
         {
-            e=mid-1;
+            e = mid - 1;
         }
         else
         {
-            s=mid+1;
+            s = mid + 1;
         }
     }
     return -1;
 }
 int main()
 {
-    int arr[]={1,2,3,4,5,6,7,8,9,10};
-    int n=sizeof(arr)/sizeof(int);
-   
-    if(search(arr,n,6)==-1)
-    {cout<<"not found"<<endl;}
-    else{cout<<"found at "<<search(arr,n,6)+1<<endl;}
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int n = sizeof(arr) / sizeof(int);
+
+    if (search(arr, n, 6) == -1)
+    {
+        cout << "not found" << endl;
+    }
+    else
+    {
+        cout << "found at " << search(arr, n, 6) + 1 << endl;
+    }
     return 0;
 }
