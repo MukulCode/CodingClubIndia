cfaf49b1ee13556cdfc57041a021fa53cba3159f
unknown <vimarshjain321@gmail.com>
2022-10-19 01:17:13 +0530

    Contributed in Two Sum problem

--- LeetCode_Java_Solutions/Two-Sum.java
+++ LeetCode_Java_Solutions/Two-Sum.java
@@ -1,18 +1,25 @@
-//solved using hashmap 
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map <Integer, Integer> map = new HashMap<>();
-        
-        for(int i=0;i<=nums.length; i++) {
-            int compliment = target - nums[i];
-            
-            if(map.containsKey(compliment)) {
-                return new int[] {map.get(compliment), i};
-            }
-            
-            map.put(nums[i], i);
+        int arr[] = new int[2];
+        Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; ++i) {
+            mp.put(nums[i], i);
         }
-        throw new IllegalArgumentException("No match found");
+        
+        for(int j = 0; j < nums.length; ++j) {
+            int comp = target - nums[j];
+            if(mp.containsKey(comp)) {
+                int val = mp.get(comp);
+                if(val == j) {
+                    continue;
+                }
+                else {
+                    arr[0] = j;
+                    arr[1] = val;
+                    break;
+                }
+            }
+         }
+        return arr; 
     }
 }
