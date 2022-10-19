718123448f25881cecfdaa16d4fad5cedb582a47
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-10-19 14:34:52 +0530

    Another Square Pattern having nth number in the first column and then decrementing it by 1

---
+++ Pattern10.cpp
@@ -0,0 +1,28 @@
+/* 
+    4321
+    4321
+    4321
+    4321
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int i=1;
+    while(i<=n)
+    {
+        int j=1;
+        while(j<=n)
+        {
+            cout<<n-j+1;
+            j++;
+        }
+    cout<<endl;
+    i++;
+    }
+}
