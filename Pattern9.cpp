f3386decb5deb734dd9b498ced489bef204078bc
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-10-19 14:32:53 +0530

    Square Pattern following Column Number

---
+++ Pattern9.cpp
@@ -0,0 +1,28 @@
+/* 
+    1234
+    1234
+    1234
+    1234
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int i=1;
+    while(i<=n)
+    {
+        int j=1;
+        while(j<=n)
+        {
+            cout<<j;
+            j++;
+        }
+    cout<<endl;
+    i++;
+    }
+}
