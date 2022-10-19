e05680ef11e92a96fc6d07a45e159e12d5f760dc
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-10-19 14:23:14 +0530

    Added upper triangle problem

---
+++ Pattern3.cpp
@@ -0,0 +1,28 @@
+/*
+    ****
+    ***
+    **
+    *               
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int i = 1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n - i + 1)
+        {
+            cout << "*";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+}
